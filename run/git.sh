80dfe76066bc97e7d1d98391e66bc6b37e7ad9e6
withpulp <sajjad@withulp.com>
2015-03-27 01:45:01 -0400

    gitty gitty git

---
+++ run/git.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Colors!
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[1;33m'
+nc='\033[0m' # No Color
+
+# If initializing remotes
+if [ "$1" = "init" ]
+then
+  # init : Use $FUNCNAME
+  drunk(){
+    git remote add $2
+    git remote add $3
+    git remote add $4
+    git remote add $5
+  }
+  init $1 $2 $3 $4 $5
+  echo -e "gitty added\n$2\n$3\n$4\n$5\nremoties >:P"
+
+# If pushing to all repos
+elif [ "$1" = "shove" ]
+then
+  # shove : Use $FUNCNAME
+  shove(){
+    git push adcade
+    git push saucey
+    git push withpulp
+    git push sajjad
+  }
+  shove
+  echo -e "gitty shoved to remoties >:P"
+
+else
+  echo -e "${red}ERROR: ${red}Check your statement. You can only use init -or- shove${nc}"
+fi
