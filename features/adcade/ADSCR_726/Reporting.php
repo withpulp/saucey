b6e9144acd33c2d5e7c77d86ea3841bd0be0fe1b
Sajjad Hossain <sajjad@adcade.com>
2015-05-05 11:28:32 -0400

    Removed other suites from HelpCenter branch

--- features/adcade/ADSCR_726/Reporting.php
+++
@@ -1,3 +0,0 @@
-<?php
-rename("./reports/saucey_report.html", "./reports/ADSCR_726/ADSCR726_Report_".time().".html");
-?>
--- features/adcade/ADSCR_726/Run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for ((n=0;n<100;n++))
-do
- bin/robo adcade:adscr726
-done
